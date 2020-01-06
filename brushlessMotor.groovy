5cfff8c5fb3717375b527cec9d056c7a9ddb5fac
Kevin Harrington <mad.hephaestus@gmail.com>
2020-01-05 22:17:59 -0500

    new CAD loader script

---
+++ brushlessMotor.groovy
@@ -0,0 +1,20 @@
+import eu.mihosoft.vrl.v3d.parametrics.*;
+CSG generate(){
+	String type= "brushlessMotor"
+	if(args==null)
+		args=["TL2955 "]
+	// The variable that stores the current size of this vitamin	StringParameter size = new StringParameter(	type+" Default",args.get(0),Vitamins.listVitaminSizes(type))
+	HashMap<String,Object> measurments = Vitamins.getConfiguration( type,size.getStrValue())
+
+	def diameterValue = measurments.diameter
+	def boltSizeValue = measurments.boltSize
+	def heightValue = measurments.height
+	println "Loaded from vitamins measurments: diameterValue" value is = diameterValue
+	println "Loaded from vitamins measurments: boltSizeValue" value is = boltSizeValue
+	println "Loaded from vitamins measurments: heightValue" value is = heightValue
+	// Stub of a CAD object	CSG part = new Cube().toCSG()   
+return part
+		.setParameter(size)
+		.setRegenerate({generate()})
+}
+return generate() 
